0bfd8c50c44328b83d8f511d2a313c2d4732505b
Ahmet Talha Köylü <ahmettalha.net@gmail.com>
2021-08-18 12:26:22 +0300

    Router yapısı eklendi

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
 import './assets/scss/index.scss'
-import {library} from '@fortawesome/fontawesome-svg-core'
-import {fas} from '@fortawesome/free-solid-svg-icons'
-import {far} from '@fortawesome/free-regular-svg-icons'
-import {fab} from '@fortawesome/free-brands-svg-icons'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
 import { Alert } from "react-bootstrap";
 import Header from './components/partials/Header';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './pages/Home';
 library.add(fab, fas, far)
 
 function App() {
   return (
     <div className="App">
-      <Header></Header>
+      <Router>
+        <Header></Header>
+        <Switch>
+          <Route path="/authors">
+            authors
+          </Route>
+          <Route path="/books">
+            books
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/components/partials/Header.jsx
+++ src/components/partials/Header.jsx
@@ -1,25 +1,24 @@
 import { Navbar, Container, NavDropdown, Nav, Button } from 'react-bootstrap'
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import Categories from '../CategoryMenu'
+import Categories from '../Nav/CategoryMenu'
+import { Link } from 'react-router-dom'
+import NavMenu from './../Nav/NavMenu';
 
 export default function Header() {
     return (
         <div className="mb-4">
             <Navbar bg="light" expand="lg">
                 <Container className="py-3">
-                    <Navbar.Brand href="#home" className={"fw-bold"}>
-                        <FontAwesomeIcon icon={"book"} className={"me-2"} />
-                        Bookido
-                    </Navbar.Brand>
+                    <Link to="/">
+                        <Navbar.Brand to="/" className={"fw-bold"}>
+                            <FontAwesomeIcon icon={"book"} className={"me-2"} />
+                            Bookido
+                        </Navbar.Brand>
+                    </Link>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="me-auto ms-auto">
-                            <Nav.Link href="#home">
-                                <FontAwesomeIcon icon={"home"} className="me-2"></FontAwesomeIcon>
-                                Ana Sayfa
-                            </Nav.Link>
-                        </Nav>
+                        <NavMenu />
                         <div className="d-flex">
                             <Button>
                                 <FontAwesomeIcon icon={"user"} className="me-2"></FontAwesomeIcon>
